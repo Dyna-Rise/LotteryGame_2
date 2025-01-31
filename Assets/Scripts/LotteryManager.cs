1677185bef69f6d4538cf4af567c2a069c77b1cd
IRIE\uc_hu <uc_huiu-.-ubi112233@outlook.com>
2025-01-31 10:22:40 +0900

    0130 Lottery

---
+++ Assets/Scripts/LotteryManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LotteryManager : MonoBehaviour
+{
+    public OptionData optionData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.isSpinnig == true)
+        {
+            if (GameManager.isLottery == false)
+            {
+                GameManager.isLottery = true;
+                GameManager.isResult = false;
+                StartLottery();
+            }
+        }
+    }
+
+
+    public void StartLottery()
+    {
+        GameManager.resultIndex = Random.Range(0, optionData.option.prizeName.Length);
+        Debug.Log("íäëIî‘çÜåãâ  : " + GameManager.resultIndex);
+        GameManager.isResult = true;
+    }
+
+}   
