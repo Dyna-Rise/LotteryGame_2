c881df025feb1c51ebd7cb875c093fda23b20cfd
Dyna-Rise <dynarise.headquarters@gmail.com>
2025-01-31 11:44:23 +0900

    0130 Title

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject[] reels;
+    public float reelSpeed = 50.0f;
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0;i< reels.Length;i++)
+        {
+            reels[i].transform.Rotate(-reelSpeed*Time.deltaTime,0,0);
+        }        
+    }
+
+    public void TitleChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
